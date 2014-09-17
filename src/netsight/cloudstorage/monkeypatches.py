3eb851fbccf98e40762eb91efa4e0e90ea67eecb
Ben Cole <benc@netsight.co.uk>
2014-09-17 10:46:45 +0100

    Added monkey patches

---
+++ src/netsight/cloudstorage/monkeypatches.py
@@ -0,0 +1,14 @@
+"""
+Monkeypatch FileField and BlobField download methods to allow returning
+cloud based URL
+"""
+from netsight.cloudstorage.interfaces import ICloudStorage
+
+
+def file_field_download(self, instance, **kwargs):
+    fieldname = self.getName()
+    adapter = ICloudStorage(instance)
+    url = adapter.get_url(fieldname)
+    if url is not None:
+        return instance.REQUEST.RESPONSE.redirect(url)
+    return self._old_download(self, instance, **kwargs)
