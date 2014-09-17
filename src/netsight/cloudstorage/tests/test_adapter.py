e98e3d26c9cd4b1fe395882e8e339878cc9a6c2c
Ben Cole <benc@netsight.co.uk>
2014-09-17 10:46:51 +0100

    WiP tests

---
+++ src/netsight/cloudstorage/tests/test_adapter.py
@@ -0,0 +1,39 @@
+from plone import api
+
+from .base import BaseTestCase
+from ..interfaces import ICloudStorage
+
+
+class TestCloudStorage(BaseTestCase):
+
+    def setUp(self):
+        super(TestCloudStorage, self).setUp()
+        self.login_as_portal_owner()
+        self.a_file = api.content.create(
+            self.portal,
+            'File',
+            'example-file',
+            'file.txt'
+        )
+        self.doc = api.content.create(
+            self.portal,
+            'Document',
+            'example-document',
+            'A document'
+        )
+
+    def test__getFields(self):
+        fields = ICloudStorage(self.a_file)._getFields()
+        self.assertGreater(len(fields), 0)
+        fields = ICloudStorage(self.doc)._getFields()
+        self.assertEqual(len(fields), 0)
+
+    def test_enqueue(self):
+        """
+        Not actually testing Celery or AWS.
+
+        1. enqueue an object
+        2. check that it has "in_progress" fields
+
+        """
+        self.fail()
