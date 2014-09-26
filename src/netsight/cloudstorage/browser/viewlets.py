eb37670bb3df641e1255150893f032b88316da2f
Matthew Sital-Singh <mattss@netsight.co.uk>
2014-09-26 09:43:42 +0100

    Tweaks to viewlet messages

--- src/netsight/cloudstorage/browser/viewlets.py
+++ src/netsight/cloudstorage/browser/viewlets.py
@@ -21,18 +21,21 @@ class StatusViewlet(ViewletBase):
 
     def status_message(self):
         if self.adapter.has_in_progress_uploads():
-            return _(u'Some content on this page is currently being uploaded '
-                     u'to the cloud')
+            return _(u'File data for this item is currently being uploaded '
+                     u'to secure cloud storage')
         elif self.adapter.has_uploaded_all_fields():
-            return _(u'Content on this page is being served from cloud '
-                     u'storage')
+            return _(u'File data for this item is being served from'
+                     u'secure cloud storage')
         else:
-            return _(u'Some fields can be uploaded to the cloud.')
+            return _(u'File data for this item can be uploaded to'
+                     u'secure cloud storage')
 
     def upload_possible(self):
         in_progress = self.adapter.has_in_progress_uploads()
         uploaded_all = self.adapter.has_uploaded_all_fields()
-        if in_progress is False and uploaded_all is False and self.available() is True:
+        if in_progress is False and \
+           uploaded_all is False and \
+           self.available() is True:
             return True
         return False
 
