ea213c3d4b0aa9f7b66885c8c2249e16d6b19e57
Ben Cole <benc@netsight.co.uk>
2014-10-20 14:22:20 +0100

    Added - docstrings

--- src/netsight/cloudstorage/adapters.py
+++ src/netsight/cloudstorage/adapters.py
@@ -1,6 +1,9 @@
 # -- The contents of this file is copyright (c) 2011 Netsight Internet     -- #
 # -- Solutions Ltd. All rights reserved. Please see COPYRIGHT.txt and      -- #
 # -- LICENCE.txt for further information.                                  -- #
+"""
+The adapters for CloudStorage
+"""
 import logging
 from uuid import uuid4
 
